6e1862c5a0b43b013ea44cf8cc0bba4f5ab0d1f0
SudarshanSaxena <sudarshan.github@gmail.com>
2023-08-15 21:43:35 +0530

    initial commit

---
+++ Arrays and Strings/1.1.js
@@ -0,0 +1,18 @@
+// Algo to determine if all charaters are unique in a string
+
+function checkIfUnique(string) {
+  let duplicateFlag = false
+  const hashMap = new Map()
+
+  for (let char of string) {
+    hashMap.set(char, (hashMap.get(char) || 0) + 1)
+  }
+
+  for (const [key, value] of hashMap) {
+    if (value > 1) duplicateFlag = true
+  }
+
+  return duplicateFlag
+}
+
+console.log(checkIfUnique('abca'))
