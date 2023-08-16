41f5403f47a5c0198206da14a9130fc23583b1f9
SudarshanSaxena <sudarshan.github@gmail.com>
2023-08-16 23:48:04 +0530

    fix

--- Arrays and Strings/substring.js
+++ Arrays and Strings/substring.js
@@ -3,13 +3,16 @@
 function checkIfSubString(string1, string2) { // string2 will always be the smaller one or equal one
   let left = 0
   let right = string2.length
+  let index = 0
 
   while (left < right) {
-    if (string1.charAt(left) != string2.charAt(left)) {
+    if (string1.charAt(left) != string2.charAt(index)) {
       left++
       right = left + string2.length
+      index = 0
     } else {
       left++
+      index++
     }
 
     if (left == right) {
