536029a84d5d20772e43fbe69988eb13d3a462ba
SudarshanSaxena <sudarshan.github@gmail.com>
2023-08-18 23:58:18 +0530

    creting a linked list

--- Linked list/node.js
+++ Linked list/node.js
@@ -4,3 +4,13 @@ class Node {
     this.next = null
   }
 }
+
+const a = new Node("A")
+const b = new Node("B")
+const c = new Node("C")
+const d = new Node("D")
+
+
+a.next = b
+b.next = c
+c.next = d
